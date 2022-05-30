62b9296408ba964e8e9c809eeaccf43b0c45bdcc
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-05-31 01:59:02 +0900

    Initial Commit

--- judge_obsolete_inuse/valid.py
+++ judge_obsolete_inuse/valid.py
@@ -42,7 +42,7 @@ def visualize_model(model, valid_dataloader: DataLoader, valid_dataset: Dataset_
         print(class_names[labels])
         outputs = model(inputs) # 訓練後のNNに画像を入力
         _, predicted = torch.max(outputs, 1) # #入力した画像の行列の最大値（もっとも確率の高いもの）を返す
-
+        print(outputs)
         print('='*20)
 
         
